a238276490b3a326a67f97954480c416f75fc408
Yann KAISER <yann.kaiser68@gmail.com>
2025-06-22 16:11:09 +0200

    feat(core): provide config using the base config class instead of name

--- examples/auth-service/src/auth-service-web.config.ts
+++ examples/auth-service/src/auth-service-web.config.ts
@@ -1,7 +1,7 @@
 import { Config } from "@zenith-framework/core";
 import { ZenithWebConfig } from "@zenith-framework/web";
 
-@Config('ZenithWebConfig')
+@Config(ZenithWebConfig)
 export class AuthServiceWebConfig extends ZenithWebConfig {
     httpServerPort(): number {
         return 3001;
